8bcf52ecc785552109ee50d1f0d49b7dfe81e1b2
nasa9084 <nasa9084@live.jp>
2017-09-28 11:19:44 +0900

    initial commit

---
+++ totp.go
@@ -0,0 +1,27 @@
+package totp
+
+import (
+	"time"
+
+	"github.com/nasa9084/go-hotp"
+)
+
+// Generator generates Time-based One-Time Password
+type Generator struct {
+	TimeStep  uint64    // X in RFC6238
+	StartTime int64  // T0 in RFC6238, default 0 is OK
+	Secret    string // shared secret for generate hotp
+	Digit     int
+}
+
+// Generate OTP
+func (g *Generator) Generate() int64 {
+	now := time.Now().Unix()
+	t := (now - g.StartTime) / int64(g.TimeStep)
+	h := hotp.Generator{
+		Secret:  g.Secret,
+		Digit:   g.Digit,
+		Counter: uint64(t),
+	}
+	return h.Generate()
+}
