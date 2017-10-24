a69d7e0da03c28df9a0c35fa22b9885d3666effb
nasa9084 <nasa9084@live.jp>
2017-10-24 16:56:48 +0900

    fix: use UTC expressly

--- totp.go
+++ totp.go
@@ -19,7 +19,7 @@ func (g *Generator) Generate() int64 {
 	if g.TimeStep == 0 {
 		g.TimeStep = 30
 	}
-	now := time.Now().Unix()
+	now := time.Now().UTC().Unix()
 	t := (now - g.StartTime) / int64(g.TimeStep)
 	h := hotp.Generator{
 		Secret:  g.Secret,
